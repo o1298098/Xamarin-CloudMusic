abc51304c4fa62701c6213d6b868bfa0df931087
o1298098 <o1298098@live.com>
2019-05-09 21:48:58 +0800

    修改ui

--- CloudMusic/Views/BlankPage.xaml.cs
+++ CloudMusic/Views/BlankPage.xaml.cs
@@ -11,12 +11,12 @@ namespace CloudMusic.Views
         public BlankPage()
         {
             InitializeComponent();
-            //Device.StartTimer(TimeSpan.FromMilliseconds(1), ()=> { canvasview.InvalidateSurface();return true; });
+            Device.StartTimer(TimeSpan.FromSeconds(1f/60), ()=> { canvasview.InvalidateSurface();return true; });
         }
         SKPaint CirclePaint = new SKPaint
         {
-            Color=Color.Orange.ToSKColor(),
-            Style=SKPaintStyle.Fill
+            Color=Color.Accent.ToSKColor(),
+            Style=SKPaintStyle.Stroke
         };
         SKPaint linepaint = new SKPaint
         {
@@ -25,6 +25,10 @@ namespace CloudMusic.Views
             StrokeCap=SKStrokeCap.Round,
 
         };
+        SKPaint eyepaint = new SKPaint
+        {
+            Style = SKPaintStyle.Fill
+        };
         int i = 1;
         private void SKCanvasView_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs e)
         {
@@ -32,20 +36,35 @@ namespace CloudMusic.Views
             int width = e.Info.Width;
             int height = e.Info.Height;
             canvas.Clear();
-            canvas.DrawColor(Color.SkyBlue.ToSKColor());
-            float t = (DateTime.Now.Second % 2 + DateTime.Now.Millisecond / 1000f);
-            using (SKPath path = new SKPath())
+            //canvas.DrawColor(Color.SkyBlue.ToSKColor());
+            float t = (float)Math.Sin((DateTime.Now.Second % 2 + DateTime.Now.Millisecond / 1000f)*Math.PI);
+            /*using (SKPath path = new SKPath())
             {
-                SKPoint point1 = new SKPoint(width / 2+100, height / 2+200);
-                SKPoint point2 = new SKPoint(width / 2+100, height / 2+500);
-                SKPoint point3 = new SKPoint(width / 2-150, height / 2+300);
+                SKPoint point1 = new SKPoint(width / 2+50*t, height / 2+200);
+                SKPoint point2 = new SKPoint(width / 2, height / 2 + 250 - Math.Abs(50*t));
+                SKPoint point3 = new SKPoint(width / 2-150*t, height / 2 + 250 - Math.Abs(75 * t));
                 canvas.Save();
                 path.MoveTo(width / 2, height / 2);
                 path.CubicTo(point1, point2, point3);
                 canvas.DrawPath(path,linepaint);
+                canvas.Restore();
+            }*/
+            canvas.Save();
+            canvas.DrawCircle(width / 2f, height / 2f, 100, CirclePaint);
+            canvas.DrawCircle(width / 2f - 40, height / 2f - 30, 10, eyepaint);
+            canvas.DrawCircle(width / 2f + 40, height / 2f - 30, 10, eyepaint);
+            canvas.Restore();
+            using (SKPath path = new SKPath())
+            {
+                SKPoint point1 = new SKPoint(width / 2 + 50 * t, height / 2 + 200);
+                SKPoint point2 = new SKPoint(width / 2, height / 2 + 250 - Math.Abs(50 * t));
+                SKPoint point3 = new SKPoint(width / 2 - 50 * t, height / 2 + 250 - Math.Abs(75 * t));
+                canvas.Save();
+                path.MoveTo(width / 2, height / 2 + 30);
+                path.CubicTo(point1, point2, point3);
+                canvas.DrawPath(path, linepaint);
+                canvas.Restore();
             }
-
-
         }
     }
 }
